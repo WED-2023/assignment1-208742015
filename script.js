43ce7c4f41c44c37617bee49de85c66c874ccc3c
rayyan <rayanb@post.bgu.ac.il>
2024-05-16 22:34:28 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (event) => {
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+})
+const likeButtons = document.querySelectorAll('.like-btn');
+
+likeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const likeText = button.querySelector('.like-text');
+    const likeCount = parseInt(likeText.textContent.split('(')[1].slice(0, -1));
+    likeText.textContent = `Give me a Like  (${likeCount + 1})`;
+  });
+}); 
